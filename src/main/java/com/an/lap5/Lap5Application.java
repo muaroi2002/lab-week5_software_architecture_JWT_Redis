c3482dde8305c3796a779f2559c1d3694c1e4738
muaroi2002 <muaroi2002@gmail.com>
2024-03-08 15:37:26 +0700

    Add Pj
    create project

---
+++ src/main/java/com/an/lap5/Lap5Application.java
@@ -0,0 +1,13 @@
+package com.an.lap5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lap5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lap5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/an/lap5/Lap5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.an.lap5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lap5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
